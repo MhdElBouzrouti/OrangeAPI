c608eb0870d0f1b32ccc0c0406721f1d6a88866a
Mohammed EL BOUZROUTI <Mohammedelbouzrouti@gmail.com>
2016-05-08 19:55:46 +0200

    Back office

--- .tmp/public/back/js/controllers/DashboardController.js
+++ .tmp/public/back/js/controllers/DashboardController.js
@@ -7,7 +7,7 @@ angular.module('MetronicApp').controller('DashboardController', function($rootSc
     $rootScope.settings.layout.pageContentWhite = true;
     $rootScope.settings.layout.pageBodySolid = false;
     $rootScope.settings.layout.pageSidebarClosed = false;
-  $scope=users=[];
+  $scope.users=[];
   $http({
     method:'GET',
     url:'/user'
